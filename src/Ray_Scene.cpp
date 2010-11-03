d0f111498dd78350a871a7a9857f3db391adf14e
dzd <dzd2009@gmail.com>
2010-11-04 00:41:15 +0100

    still buggy...

--- src/Ray_Scene.cpp
+++ src/Ray_Scene.cpp
@@ -18,11 +18,7 @@ Scene::Scene()
 void Scene::GenerateScene()
 {
     RenderableObject * r;
-    //Temporary hardcoded init of renderable object list
-     Color c(0, 255, 0);
-     c.Show();
-     r = new RenderableObject(c, new RSphere(Point(300,240,300), 200));
-     ObjectList.push_back(r);
+
 
     Color c2(255, 0, 0);
     r = new RenderableObject(c2, new RSphere(Point(100,100,190), 166));
@@ -33,8 +29,14 @@ void Scene::GenerateScene()
     r = new RenderableObject(c3, new RSphere(Point(600,380,170), 166));
     ObjectList.push_back(r);
 
-    // Append a new light to the scene
-    Light l = Light(Point(320,240,0), Color(255,255,255));
+    //Temporary hardcoded init of renderable object list
+     Color c(0, 255, 0);
+     c.Show();
+     r = new RenderableObject(c, new RSphere(Point(300,240,300), 200));
+     ObjectList.push_back(r);
+
+     // Append a new light to the scene
+    Light l = Light(Point(600,380,50), Color(255,255,255));
     LightList.push_back(l);
 }
 
@@ -61,6 +63,7 @@ void Scene::Render()
         r = new Ray(camera->GetDirection(), *sp);
 
         list<RenderableObject*>::iterator it = ObjectList.begin();
+        list<RenderableObject*>::iterator it_intersection_obj;
         for (;it != ObjectList.end(); it++)
         {
             distance = -1;
@@ -72,13 +75,16 @@ void Scene::Render()
                     //cout << "Closer intersection found at: "<< distance << "for ray: " << *r << endl;
                     sp->SetDistance(distance);
                     sp->SetColor( (*it)->GetColor() );
+                    // save object which is the closer of the screen
+                    it_intersection_obj = it;
                 }
             }
         }
         //now iterate throught scene's light list to compute pixel color
-        //if (distance == -1 ) break;
+        if (distance == -1 ) continue;
 
         list<Light>::iterator light_it = LightList.begin();
+
         for(;light_it != LightList.end(); light_it++)
         {
             // get the Point of intersection (we have the vector and the distance)
@@ -88,7 +94,7 @@ void Scene::Render()
                                          distance * r_vector->Y() + r_orig->Y(),
                                          distance * r_vector->Z() + r_orig->Z());
             // get a unit vector with intersection and light source
-            Vector light_vector = Vector((*light_it).GetPosition(), p_intersection);
+            Vector light_vector = Vector(p_intersection, (*light_it).GetPosition());
             
             // create a ray with the intersection and the light point
             delete r;
@@ -98,18 +104,24 @@ void Scene::Render()
             list<RenderableObject*>::iterator it_obj_light = ObjectList.begin();
             for (;it_obj_light != ObjectList.end(); it_obj_light++)
             {
-                if ((*it_obj_light)->geo->GetIntersection(*r, distance2))
+                //do not check intersection with the current object found as the closest intersection of the ray.
+                if (it_obj_light != it_intersection_obj )
                 {
-                    //cout << "Intersection found !, pixel in a shadow" << endl;
-                    pixelInShadow = true;
-                    break;
+                    if ((*it_obj_light)->geo->GetIntersection(*r, distance2))
+                    {
+                        //cout << "Intersection found !, pixel in a shadow" << endl;
+                        pixelInShadow = true;
+                        break;
+                    }
                 }
             }
             // if no intersection, update ScreenPoint Color with light
             if ( pixelInShadow )
             {
                 cout << "Ray in light! :" << *r << endl;
-                sp->SetColor(Color(0,0,0));
+                Color c = (*it_intersection_obj)->GetColor();
+
+                sp->SetColor(Color());
             }
             
         }
